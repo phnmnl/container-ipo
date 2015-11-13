784b742551b46bbdd1f25471d0241e86eb06ce00
Pablo Moreno <pablo.a.moreno@gmail.com>
2015-11-13 14:10:12 +0000

    Removes EBI repo setting for R packages installation.

--- scripts/installIPO.R
+++ scripts/installIPO.R
@@ -5,8 +5,8 @@ source("http://bioconductor.org/biocLite.R")
 biocLite("CAMERA")
 biocLite("xcms")
 
-install.packages("rsm", , repos='http://mirrors.ebi.ac.uk/CRAN') 
-install.packages("devtools",, repos='http://mirrors.ebi.ac.uk/CRAN')
+install.packages("rsm") 
+install.packages("devtools")
 
 library("devtools")
 install_github("glibiseller/IPO")
