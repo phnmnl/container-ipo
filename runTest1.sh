5554e1a541a361ab9123609f52122976b9cf8851
gabrielctn <gabrielcretin@gmail.com>
2018-03-29 10:41:52 +0200

    Update testing for container

--- runTest1.sh
+++ runTest1.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
 apt-get update -y && apt-get install -y --no-install-recommends wget ca-certificates
 
-# download files path and data files
-wget "https://drive.google.com/uc?export=download&id=0B7S2ZMhdzWwbMG5wOXpmU3Y1RWc" -O filesPaths_2_ms.txt
-wget "https://drive.google.com/uc?export=download&id=0B7S2ZMhdzWwbUk9LdWl3SXFwQnc" -O MSpos-Ex1-Col0-48h-Ag-1_1-A,1_01_9818.mzData
-wget "https://drive.google.com/uc?export=download&id=0B7S2ZMhdzWwbUk90c1Rva0Fibnc" -O MSpos-Ex1-Col0-48h-Ag-2_1-A,1_01_9820.mzData 
-mkdir contTest
+# download test data file
+wget "https://raw.githubusercontent.com/workflow4metabolomics/ipo/master/galaxy/ipo/test-data/MM8.mzML" -O MM8.mzML
 
-runIPO.R -i filesPaths_2_ms.txt -o contTest -s 2
+# Peak picking first
+ipo4xcmsSet.r singlefile_galaxyPath /usr/local/bin/MM8.mzML singlefile_sampleName MM8.mzML parametersOutput MM8_IPO_parameters4xcmsSet.tsv nSlaves 4
+
+# Then Grouping
+ipo4retgroup.r image raw/resultPeakpicking.RData parametersOutput MM8_IPO_parameters4retgroup.tsv method density singlefile_galaxyPath /usr/local/bin/MM8.mzML singlefile_sampleName MM8.mzML retcorMethod obiwarp nSlaves 4
 
 # check that JPGs graphs have been created
-for index in `seq 1 4`; do
-	if [ ! -f contTest/rsm_$index.jpg ]; then
+for index in `seq 1 2`; do
+	if [ ! -f IPO_results/rsm_$index.jpg ]; then
 		echo "JPG graph rsm_$index.jpg not found, failing test"
+		exit 1
+	fi
+	if [ ! -f IPO_results/retgroup_rsm_$index.jpg ]; then
+		echo "JPG graph retgroup_rsm_$index.jpg not found, failing test"
 		exit 1
 	fi
 done
 
-echo "massIPO runs with test data without error codes, all expected files created."
+# check that RData files have been created
+if [ ! -f raw/resultPeakpicking.RData ] || [ ! -f raw/resultRetCor.RData ]; then
+	echo "RData files not found, failing test"
+	exit 1
+fi
 
+# check that tsv files have been created
+if [ ! -f raw/MM8_IPO_parameters4xcmsSet.tsv ] || [ ! -f raw/MM8_IPO_parameters4retgroup.tsv ]; then
+	echo "tsv files not found, failing test"
+	exit 1
+fi
+
+echo "massIPO runs with test data without error codes, all expected files created."
