902a85ca93b98130494cf17a14bf570fe117fb90
gabrielctn <gabrielcretin@gmail.com>
2018-04-01 15:26:21 +0200

    Use MM14 data file instead of MM8 + use range of values for peakwidth

--- runTest1.sh
+++ runTest1.sh
@@ -2,16 +2,16 @@
 apt-get update -y && apt-get install -y --no-install-recommends wget ca-certificates
 
 # download test data file
-wget "https://raw.githubusercontent.com/workflow4metabolomics/ipo/master/galaxy/ipo/test-data/MM8.mzML" -O "/usr/local/bin/MM8.mzML"
+wget "https://raw.githubusercontent.com/workflow4metabolomics/ipo/master/galaxy/ipo/test-data/MM14.mzML" -O "/usr/local/bin/MM14.mzML"
 
 # Peak picking first
-ipo4xcmsSet.r singlefile_galaxyPath /usr/local/bin/MM8.mzML singlefile_sampleName MM8.mzML parametersOutput MM8_IPO_parameters4xcmsSet.tsv nSlaves 4
+ipo4xcmsSet.r singlefile_galaxyPath /usr/local/bin/MM14.mzML singlefile_sampleName MM14.mzML parametersOutput MM8_IPO_parameters4xcmsSet.tsv nSlaves 4 minPeakWidth 3,9.5 maxPeakWidth 10,20 ppm 56
 
 # Then Grouping
-ipo4retgroup.r image raw/resultPeakpicking.RData parametersOutput MM8_IPO_parameters4retgroup.tsv method density singlefile_galaxyPath /usr/local/bin/MM8.mzML singlefile_sampleName MM8.mzML retcorMethod obiwarp nSlaves 4
+ipo4retgroup.r image raw/resultPeakpicking.RData parametersOutput MM14_IPO_parameters4retgroup.tsv method density singlefile_galaxyPath /usr/local/bin/MM14.mzML singlefile_sampleName MM14.mzML retcorMethod obiwarp nSlaves 4
 
 # check that JPGs graphs have been created
-for index in `seq 1 2`; do
+for index in `seq 1 3`; do
 	if [ ! -f IPO_results/rsm_$index.jpg ]; then
 		echo "JPG graph rsm_$index.jpg not found, failing test"
 		exit 1
@@ -29,7 +29,7 @@ if [ ! -f raw/resultPeakpicking.RData ] || [ ! -f raw/resultRetCor.RData ]; then
 fi
 
 # check that tsv files have been created
-if [ ! -f raw/MM8_IPO_parameters4xcmsSet.tsv ] || [ ! -f raw/MM8_IPO_parameters4retgroup.tsv ]; then
+if [ ! -f raw/MM14_IPO_parameters4xcmsSet.tsv ] || [ ! -f raw/MM14_IPO_parameters4retgroup.tsv ]; then
 	echo "tsv files not found, failing test"
 	exit 1
 fi
